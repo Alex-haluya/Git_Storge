e4b18b837101c6ccec322faf50291583c4a74aea
Konoha <misskonohasakura@gmail.com>
2020-10-05 09:08:44 +0800

    Test Try

---
+++ panduankohaoshifoupipei.cpp
@@ -0,0 +1,27 @@
+#include<cstdlib>
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main(){
+    int n = 5;
+    int a[] = {2, 1, 5, 3, 4};
+    bool flag = true;
+    for (int i = 0; i < n-1; i++)
+    {
+        if ((a[i] > a[i+1]) && (a[i]-1 != a[i+1]))
+        {
+            flag = false;
+            break;
+        }
+    }
+    if (flag)
+    {
+        cout << "Legal";
+    } else
+    {
+        cout << "Illegal";
+    }
+    system("PAUSE");    
+    return 0;
+}
